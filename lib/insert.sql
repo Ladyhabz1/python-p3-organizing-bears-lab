6cb9b378a14e48821abc680f70e8ca3d9c7c546f
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-01 02:49:12 +0300

    completing labs

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'black', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 30, 'M', 'white', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'red', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'black white', 'warm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'brown', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 10, 'M', 'green', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'gray', 'warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'M', 'black', 'warm', 1);
